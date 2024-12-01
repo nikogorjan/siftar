4f7eadf8dda3eb587f6d062d20fdae2ce276c54f
nikogorjan <niko.gorjan@gmail.com>
2024-12-01 16:17:31 +0100

    gostisce add

---
+++ src/app/gostisce-siftar/page.js
@@ -0,0 +1,14 @@
+import Navbar from "../../../public/Components/Navbar/Navbar";
+import Footer from "../_components/Footer/Footer";
+
+
+export default function Home() {
+  return (
+    <main>
+      <div className='thisstupis'>
+      <Navbar/>
+      </div>
+      <footer><Footer/></footer>
+    </main>
+  );
+}
